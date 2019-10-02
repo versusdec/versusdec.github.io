b4c2c34fa29c6fa8c122e50dee251d4022cd82de
versusdec <versusdec@gmail.com>
2019-10-02 21:31:50 +0300

    adaptive complete

--- alexmotors/js/main.js
+++ alexmotors/js/main.js
@@ -2,8 +2,8 @@ $(function () {
 
   const catalogSwiper = new Swiper('.catalog-slider', {
     loop: true,
-    slidesPerView: 5,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 40,
     pagination: {
       el: '.swiper-pagination',
     },
@@ -11,13 +11,20 @@ $(function () {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
+    breakpoints: {
+      769: {
+        slidesPerView: 2
+      },
+      1001: {
+        slidesPerView: 3
+      }
+    }
   });
 
   const carsSlider = new Swiper('.cars-slider', {
     loop: true,
-    slidesPerView: 2,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 40,
     pagination: {
       el: '.swiper-pagination',
     },
@@ -25,7 +32,21 @@ $(function () {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      769: {
+        slidesPerView: 2
+      }
+    }
+  });
 
-  })
-
+  $('.menu-btn').on('click', function () {
+    const $menu = $('header nav');
+    const $btn = $(this);
+    $menu.toggleClass('active');
+    $(document).on('click', function (event) {
+      if (!$(event.target).closest($menu).length && !$(event.target).closest($btn).length) {
+        $menu.removeClass('active')
+      }
+    })
+  });
 });
