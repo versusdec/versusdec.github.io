db593a6dafe23b598c8365a47c275142f1b63882
versusdec <versusdec@gmail.com>
2024-01-19 15:21:50 +0200

    added guards

---
+++ _next/static/chunks/pages/index-09d9ffaf4d01f04c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5557:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(2304)}])},2304:function(n,u,_){"use strict";_.r(u);var t=_(3917);u.default=t.default}},function(n){n.O(0,[765,917,774,888,179],function(){return n(n.s=5557)}),_N_E=n.O()}]);
