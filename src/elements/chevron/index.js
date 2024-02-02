21230817fb90c099822e538fd9199cd55a79dc92
versusdec <versusdec@gmail.com>
2024-02-02 14:52:04 +0200

    webpack test

--- src/elements/chevron/index.js
+++
@@ -1,8 +0,0 @@
-
-export const Chevron = (props) => {
-  
-  return (
-    <div {...props}><img src="/assets/icons/chevron.png" alt={props.alt || 'back'}/></div>
-  )
-}
-
--- src/hoc/with-quiz-guard.js
+++
@@ -1,9 +0,0 @@
-import { QuizGuard } from '../guards/quiz-guard'
-
-export const withQuizGuard = function (Component) {
-  return function guard (props) {
-    return <QuizGuard>
-      <Component {...props} />
-    </QuizGuard>
-  }
-}
