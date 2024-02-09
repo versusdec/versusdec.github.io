d83d7e4354567f197c237ea1c11f1f9278386d3b
versusdec <versusdec@gmail.com>
2024-02-09 15:06:25 +0200

    plerdy init

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(function () {
+  const swiper = new Swiper('.swiper', {
+    spaceBetween: 17,
+    autoplay: true,
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 4
+      },
+       768: {
+         slidesPerView: 2
+       }
+    }
+  });
+  
+  setTimeout(() => {
+    $('.modal').addClass('active')
+  }, 2000)
+  
+  $('.modal .close').on('click', () => {
+    $('.modal').removeClass('active')
+  })
+  
+  $("#phone").intlTelInput({
+    initialCountry: 'ua'
+  });
+  
+  $('form input').on('keyup', e => {
+    const phone = $('form input[name=phone]')[0].value;
+    const name = $('form input[name=name]')[0].value;
+    if(phone.length > 10 && name !== '') $('form button[type=submit]').attr('disabled', false)
+    else $('form button[type=submit]').attr('disabled', true)
+  })
+  
+  $('.menu-btn').on('click', ()=>{
+    $('body')[0].toggleAttribute('data-menu')
+  })
+  
+})
